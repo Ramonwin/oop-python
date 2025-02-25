33ed237acf6ba7082eb16d9d558b9e83c3da6418
Ramon Shabo <ramonwin@programmer.net>
2025-02-25 15:46:05 +0700

    fix quiz

---
+++ 3_quiz_jawab.py
@@ -0,0 +1,7 @@
+absen = int(input("masukan nilai absen :"))
+tugas = int(input("masukan nilai tugas :"))
+uts = int(input("masukan nilai uts :"))
+uas = int(input("masukan nilai uas :"))
+
+nilaiAkhir = (absen * 0.1) + (tugas * 0.2) + (uts * 0.3) + (uas * 0.4)
+print("Nilai Akhir adalah : ", nilaiAkhir)
