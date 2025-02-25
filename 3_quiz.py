07c573dabe5000b6e51ca5dc34e1fab3a4b82586
Ramon Shabo <ramonwin@programmer.net>
2025-02-25 15:42:46 +0700

    fix quiz

--- 3_quiz.py
+++ 3_quiz.py
@@ -1,6 +1,10 @@
 quiz ="""
 1. buatlah program hitung nilai akhir dengan ketentuan
 2. semua nilai di input dinamis
-3. konversi untuk nilai absen 10%, tugas 20%, uts 30%, uas 40%
-4. munculkan pesan "Nilai akhir adalah : (variabel) "
+3. masukan nilai absen :
+4. masukan nilai tugas :
+5. masukan nilai uts :
+6. masukan nilai uas :
+7. konversi nilai absen 10%, tugas 20%, uts 30%, uas 40%
+8. munculkan pesan "Nilai akhir adalah : (variabel) "
 """
