c26805da0420e247f9e321eb0fa1c5338c5c2210
Ramon Shabo <ramonwin@programmer.net>
2025-03-06 07:22:44 +0700

    fix

--- 5.forloop.py
+++ 5.forloop.py
@@ -2,22 +2,22 @@
 for i in range(10):
     print("posisi i indeks ke -:", i)
 
-for i in range(1,5):
-    print(i)
+
+for i in range(1,11): print(i)
 
 print("===========================")
 
 # looping menggunakan list
-nilai =[1,2,3,4,5]
-for i in nilai:
-    print(i)
+# nilai =[1,2,3,4,5]
+# for i in nilai:
+#     print(i)
 
-#list ke-2
-nilai =[70,85,60,40,50]
-for i in nilai:
-    print(i)
+# #list ke-2
+# nilai =[70,85,60,40,50]
+# for i in nilai:
+#     print(i)
 
-#looping menggunakan str
-nama = "stimik amik"
-for i in nama:
-    print(i)
+# #looping menggunakan str
+# nama = "stimik amik"
+# for i in nama:
+#     print(i)
--- 5_quiz1_forloop.py
+++ 5_quiz1_forloop.py
@@ -18,4 +18,5 @@ elif tanggal == "genap":
     for i in range(1,32):
         if i % 2 == 0: print(i)
         if i == 20 : print("Pembayaran THR")
-        
+else:
+    print("Inputan tidak sesuai")        
