c2726f04a61bc2d436b663c67974b214aa02c654
Ramon Shabo <ramonwin@programmer.net>
2025-03-11 07:24:06 +0700

    while loop

---
+++ 6_whileloop.py
@@ -0,0 +1,18 @@
+# while loop
+
+# while kondisi:
+#     aksi 1
+#     aksi 2
+
+# akhir dari program
+
+angka = 1
+print(f"angka sekarang => {angka}")
+print("angka sekarang =>", angka)
+
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang = {angka}")
+
+
+
