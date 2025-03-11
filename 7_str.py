cc605a3ce6c919e4a8e52017e7efc6dcd5fb44de
Ramon Shabo <ramonwin@programmer.net>
2025-03-11 09:13:21 +0700

    rev str

--- 7_str.py
+++ 7_str.py
@@ -5,8 +5,10 @@ print(nama)
 print(nama[7:11])
 
 for i in nama:
-    print("".join(i))
+    print(i)
+    #print(i, end="") #print kesamping
 
+print("\n")
 print(f"{nama}\n", end="")
 print("".join(nama))
 
