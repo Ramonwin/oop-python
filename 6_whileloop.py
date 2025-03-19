55b6da0f4d52585702089354eb3d4f27f151bdef
Ramon Shabo <ramonwin@programmer.net>
2025-03-19 16:00:07 +0700

    tuple

--- 6_whileloop.py
+++ 6_whileloop.py
@@ -6,13 +6,11 @@
 
 # akhir dari program
 
-<<<<<<< HEAD
+
 #counted loop
 angka = 1
 print("angka sekarang =>", angka)
 #print(f"angka sekarang => {angka}")
-=======
->>>>>>> 16a974c8cbabcd606088058bbf6cec25c4dc1428
 
 #counted loop
 angka = 1
@@ -21,7 +19,7 @@ while angka <= 5:
         angka += 1
 
 #uncounted loop
-<<<<<<< HEAD
+
 password = ""
 while password != "1234":
         password = input("Masukkan password: ")
@@ -30,16 +28,5 @@ print("Password benar, Akses diterima!")
 #infinite loop
 while True:
         print("Hati -hati ini infinite loop, tidak akan berhenti!, kecuali pake break")  # Harus ada break agar bisa keluar
-=======
-password =""
-while password !="1234":
-        password =input("masukan password : ")
-print("Login Berhasil")
-
-#infinite loop
-while True:
-        print("Hati hati ini Infinite loop, tidak akan berhenti kecuali pake break")
->>>>>>> 16a974c8cbabcd606088058bbf6cec25c4dc1428
-        break
 
 
--- 9_tuple.py
+++ 9_tuple.py
@@ -11,3 +11,21 @@ print(">>> Fitur .index()")
 tuple_score = ('Budi','Sud','Budi','Budi','Ramon','Sud','Sud')
 score_pertama_sud = tuple_score.index('Ramon')
 print(score_pertama_sud) # akan menampilkan output 2
+
+#akses nilai tuple
+tuple1 = ('python','golang','php',1993,2021)
+print (tuple1)
+
+#cara seperti ini tidak bisa dilakukan, karena pada prinsipnya 
+#tuple bersifat immutable (tidak dapat diubah nilainya setelah dibuat)
+#namun ada cara alternatif yaitu dengan cara menggabungkan melalui variable baru
+
+# tuple1[1] = 'TypeScript'
+# print(tuple1) 
+
+tuple2 = ('TypeScript','JavaScript')
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+
+
