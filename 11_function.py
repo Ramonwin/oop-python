8aa7aaaf67ad66dc43b5750bd2c315ff1677a524
Ramon Shabo <ramonwin@programmer.net>
2025-04-07 15:44:59 +0700

    function

---
+++ 11_function.py
@@ -0,0 +1,53 @@
+''' Function adalah Function adalah blok kode yang bisa dipakai berulang-ulang.
+ Kita bisa memanggilnya kapan saja tanpa harus menulis ulang kodenya.'''
+
+#function ke 1
+def hello():
+    print("hallo semua")
+    print("hallo semua")
+    print("hallo semua")
+    print("hallo semua")
+
+hello()
+
+#function ke-2 dengan argument atau parameter
+def mahasiswa(nama):
+    print(f"Nama Mahasiswa : {nama}")
+
+mahasiswa("farhan")
+
+#function ke-3 dengan return (mengembalikan data)
+def salary(gapok, tunjangan):
+    hasil = gapok + tunjangan
+    return hasil
+
+print(f"take home pay salary adalah : {salary(5000000,1000000)}")
+
+#jika dicek lagi
+cek = salary(5000000,1000000)
+print(cek)
+
+# fungsi ke-4
+# perbedaan function menggunakan return dan yang menggunakan print()
+# print() yaitu langsung menampilkan output ke layar, tapi tidak bisa digunakan lagi datanya
+# Pakai print() kalau kamu cuma ingin melihat hasil (misalnya saat debugging atau testing)
+# Pakai return kalau kamu ingin mengolah hasil lebih lanjut, atau membuat program yang modular.
+def gaji(gapok, tunjangan):
+    hasil = gapok + tunjangan
+    print(f"gaji adalah : {hasil}")
+
+#jika di cek lagi
+cek = gaji(4000000,500000)
+print(cek)
+
+#fungsi ke-5, memanggil fungsi di dalam fungsi
+def sapa():
+    print("Halo!")
+
+def sapa_user(nama):
+    sapa()  # memanggil fungsi lain
+    print(f"Selamat datang, {nama}!")
+
+sapa_user("ramon")
+
+
