013ec21059e9fdaf8b1e8253ccfeb0a9dc0183f7
Ramon Shabo <ramonwin@programmer.net>
2025-05-07 17:02:39 +0700

    quiz batas atas

---
+++ 15_quiz2_inheritance.py
@@ -0,0 +1,21 @@
+quiz = """
+1. buatlah perhitungan UMK sesuai UMP yang ada di propinsi tersebut
+2. buatlah class induk UMP() dengan atribut : nilaiUMP, inflasi, index alfa
+3. buatlah method hitung_nilai_umk() di class UMP
+4. rumus umk = ump + (ump * (inflasi + alfa))
+5. buatlah class anak "UMK()" yang mewarisi class induk "UMP()"
+6. tambahan atribut pertumbuhan_ekonomi_daerah di class UMK()
+7. final umk = umk + (umk * pertumbuhan_ekonomi_daerah)
+8. tampilkan nilai UMK di kota tersebut
+9. Input nilai konsumsi perkapita
+10. Buatlah Class BatasAtas
+10. tentukan nilai batas atas dengan ketentuan
+    BatasAtas = Final_UMK + (Final_UMK * Konsumsi_perKapita)
+11. tampilkan pesan "Batas Maksimal nilai UMK kota Bandung Adalah : BatasAtas"
+
+keterangan : nilai ump = 2,5juta
+            inflasi = 10%
+            index alfa = 20%
+            pertumbuhan ed = 7%    
+            
+"""
