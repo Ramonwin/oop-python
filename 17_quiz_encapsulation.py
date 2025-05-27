9b0c69531f937938226d4b4198358594c5444bfd
Ramon Shabo <ramonwin@programmer.net>
2025-05-27 11:03:39 +0700

    quiz encapsulation

--- 17_quiz_encapsulation.py
+++ 17_quiz_encapsulation.py
@@ -3,13 +3,13 @@ Buatlah sebuah kelas bernama Student yang memiliki atribut berikut:
 
     1. name (public) → nama siswa
 
-    2. __grade (private) → nilai siswa
+    2. __nilai (private) → nilai siswa
 
 Kelas tersebut harus memiliki:
 
-    1. Method set_grade(self, grade) untuk mengatur nilai siswa (hanya jika nilai antara 0-100).
+    1. Method set_nilai(self, nilai) untuk mengatur nilai siswa (hanya jika nilai antara 0-100).
 
-    2. Method get_grade(self) untuk mengambil nilai siswa.
+    2. Method get_nilai(self) untuk mengambil nilai siswa.
 
     3. Validasi: Jika nilai tidak dalam rentang 0-100, tampilkan pesan error.
 '''
