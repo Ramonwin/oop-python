ed124ac18fe3a51b47a209bf660c2e04a473c19f
Ramon Shabo <ramonwin@programmer.net>
2025-05-27 16:11:42 +0700

    rev encapsulation

--- 17_quiz_encapsulation.py
+++ 17_quiz_encapsulation.py
@@ -13,3 +13,25 @@ Kelas tersebut harus memiliki:
 
     3. Validasi: Jika nilai tidak dalam rentang 0-100, tampilkan pesan error.
 '''
+
+class mahasiswa():
+    def __init__(self,nama):
+        self.nama = nama
+        self.__nilai = 0
+
+    def set_nilai(self, nilai):
+        if 0 <= nilai <= 100:
+            self.__nilai = nilai
+        else:
+            print ("Error : Nilai harus diantara 0 - 100")
+
+    def get_nilai(self):
+        return self.__nilai
+        
+mhs = mahasiswa("Farhan")
+# print(mhs.__nilai) ❌ AttributeError: 'Student' object has no attribute '__grade'
+
+# Ini CARA YANG BENAR (pakai method) ✅
+
+mhs.set_nilai(85)
+print(mhs.get_nilai())
